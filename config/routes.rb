e32ccc678e39acd1d17244a8d9b4e9fdec611b7b
EC2 Default User <ec2-user@ip-172-31-43-235.us-east-2.compute.internal>
2018-04-13 15:38:30 +0000

    controller_confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'messages#index'
+  
+  resources :messages
+
+  #get 'messages/:id', to: 'messages#show'
+  #post 'messages/', to: 'messages#create' 
+  #put 'messages/:id', to: 'messages#update'
+  #delete 'messages/:id', to: 'messages#delete'
+  
+  #get 'messages', to: 'messages#index'
+  #get 'messages/new', to: 'messages#new'
+  #get 'messages/:id/edit', to: 'messages#edit'
 end
